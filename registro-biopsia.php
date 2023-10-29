98c0dcc418570d562993ae904438961f00f46664
Ramón R. Bastardo M. <rrbastardom2011@hotmail.com>
2023-10-29 17:11:19 -0400

    Limpieza de formato de 'registro-citologia.html'

--- registro-biopsia.php
+++ registro-biopsia.php
@@ -8,17 +8,12 @@
 
 <html lang="es">
     <head>
-        <title>Registro de Biopsia</title>
+        <title>Registro Biopsia</title>
         
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <script src="js/jquery-3.7.1.js"></script>
     </head>
-
-
-    <!--esto del sidebar no quise modificarlo, por que aja tendriamos que ver como sera que queda eso-->
 
     <body>
         
