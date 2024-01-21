4423a804c02e78a936d763e37e3976d820ef1c42
Ramón R. Bastardo M. <rrbastardom2011@hotmail.com>
2024-01-21 11:22:20 -0400

    Codigo de Busqueda Optimizado

--- php/ajax_biopsia.php
+++ php/ajax_biopsia.php
@@ -4,35 +4,30 @@
 		$user=new CodeaDB();
 		$u=$user->buscar("m_remitido","examinado = 1 AND CI_Paciente=".$_POST['CI_Paciente']);    
 		$html=[];
+		$no_bio = 0;
 
-		$cuenta_cito = 0;
-
-		if (count($u) > 0) {
-			foreach ($u as $value){
-				$tipo_examen = $user->buscar("examen", "ID_M_remitido =".$value['ID_M_Remitido']);
-
-				foreach ($tipo_examen as $examen)
+		if($u)
+		{
+			foreach ($u as $value)
+			{
+				if($tipo_examen = $user->buscar("examen", "Tipo = 'biopsia' AND ID_M_remitido =".$value['ID_M_Remitido']))
 				{
-					if ($examen['Tipo'] == "biopsia")
+					foreach ($tipo_examen as $examen)
 					{
 						$fecha_formateada = date("d-m-Y", strtotime($examen['F_Examen']));
 						$diagnostico = $value['Descripcion_material']." - ";
 
-						$html[] = ["id"=>$examen['ID_Examen'] ,"diagnostico"=>$diagnostico, "fecha"=>$fecha_formateada ];
-					}
-					else {
-						$cuenta_cito++;
+						$html[] = ["id"=>$examen['ID_Examen'] ,"diagnostico"=>$diagnostico, "fecha"=>$fecha_formateada];
 					}
 				}
+				else {	$no_bio++;	}
 			}
 		}
-
-		if (count($u) == 0) {
-			$html[] = ["id"=>"" ,"diagnostico"=>"Paciente no posee examen", "fecha"=>"" ];
+		elseif($no_bio == count($u)) {
+			$html[] = ["id"=>"" ,"diagnostico"=>"Sin examen de biopsia", "fecha"=>""];
 		}
-		elseif ($cuenta_cito == count($u)) {
-			$html[] = ["id"=>"" ,"diagnostico"=>"Sin examen de biopsia", "fecha"=>"" ];
-		}
+		else
+		{	$html[] = ["id"=>"" ,"diagnostico"=>"Paciente no posee examen", "fecha"=>""];	}
 
 		die(json_encode($html));
 	endif;
--- php/ajax_citologia.php
+++ php/ajax_citologia.php
@@ -4,35 +4,30 @@
 		$user=new CodeaDB();
 		$u=$user->buscar("m_remitido","examinado = 1 AND CI_Paciente=".$_POST['CI_Paciente']);    
 		$html=[];
+		$no_cito = 0;
 
-		$cuenta_biop = 0;
-
-		if (count($u) > 0) {
-			foreach ($u as $value){
-				$tipo_examen = $user->buscar("examen", "ID_M_remitido =".$value['ID_M_Remitido']);
-				
-				foreach ($tipo_examen as $examen)
+		if($u)
+		{
+			foreach ($u as $value)
+			{
+				if($tipo_examen = $user->buscar("examen", "Tipo = 'citologia' AND ID_M_remitido =".$value['ID_M_Remitido']))
 				{
-					if ($examen['Tipo'] == "citologia")
+					foreach ($tipo_examen as $examen)
 					{
 						$fecha_formateada = date("d-m-Y", strtotime($examen['F_Examen']));
 						$diagnostico = $value['Descripcion_material']." - ";
 
-						$html[] = ["id"=>$examen['ID_Examen'] ,"diagnostico"=> $diagnostico, "fecha"=>$fecha_formateada ];
-					}
-					else {
-						$cuenta_biop++;
+						$html[] = ["id"=>$examen['ID_Examen'] ,"diagnostico"=>$diagnostico, "fecha"=>$fecha_formateada];
 					}
 				}
+				else {	$no_cito++;	}
 			}
 		}
-
-		if (count($u) == 0) {
-			$html[] = ["id"=>"" ,"diagnostico"=>"Paciente no posee examen", "fecha"=>"" ];
+		elseif($no_cito == count($u)) {
+			$html[] = ["id"=>"" ,"diagnostico"=>"Sin examen de citología", "fecha"=>""];
 		}
-		elseif ($cuenta_biop == count($u)) {
-			$html[] = ["id"=>"" ,"diagnostico"=>"Sin examen de citología", "fecha"=>"" ];
-		}
+		else
+		{	$html[] = ["id"=>"" ,"diagnostico"=>"Paciente no posee examen", "fecha"=>""];	}
 
 		die(json_encode($html));
 	endif;
