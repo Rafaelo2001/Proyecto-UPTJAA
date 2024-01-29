20947961a778e2b35ef886a978102423ad307494
Ramón R. Bastardo M. <rrbastardom2011@hotmail.com>
2024-01-29 00:28:29 -0400

    Ajuste a insert-patient.php: Ya no se pueden agregar pacientes repetidos & Usuarios pueden ser registrados como pacientes

--- php/insert-patient.php
+++ php/insert-patient.php
@@ -1,5 +1,8 @@
 <?php
 
+    require "../php/conexion.php";
+    $user = new CodeaDB();
+
     class BySearch
     {
         // BUSCAR x BY
@@ -59,7 +62,6 @@
             $sexo = $_POST['sexo'];
 
         // Telefono / Correo
-            // $cod_area = $_POST['cod_area'];
             $telf_paciente = $_POST['tlfn'];
             $email_paciente = $_POST['correo'];
 
@@ -98,74 +100,77 @@
 
                 }
 
+                // Confirmar que cedula persona no este registrada
+                    $buscar_ci_persona = $user->buscarExistencia("persona","CI='".$documento_id."'");                    
+
+                if(!$buscar_ci_persona){
+                            
+                    // Enviando DIRECCION
+                    $sqlDireccion = "INSERT INTO Direccion (ID_Parroquia, Localizacion, Calle, Sector, Nro_Casa) VALUES ('$parroquia', '$localizacion', '$calle', '$sector', '$nro_casa')";
+                    $ejecutadoDireccion = mysqli_query($conex,$sqlDireccion);
+                    if (!$ejecutadoDireccion) {
+                        throw new Exception("Error al insertar en la tabla Direccion" . mysqli_error($conex));
+                    }
+                            
+
+                    // Enviando PERSONA
+                    $buscarIdDirecion = new BySearch();
+                    $buscarIdDirecion->conexion = new mysqli("localhost","root","","higea_db");
+                    $resultadoDireccion = $buscarIdDirecion->buscarBY('direccion','ID_Direccion');
+                    foreach ($resultadoDireccion as $filaIdDireccion) {
+                        $idDireccion = $filaIdDireccion['ID_Direccion'];
+                        $sqlPersona = "INSERT INTO persona (CI, PN, SN, TN, PA, SA, F_nac, Edad, Sexo, ID_Direccion) VALUES ('$documento_id', '$nombre1_paciente', '$nombre2_paciente', '$nombre3_paciente', '$apellido1_paciente', '$apellido2_paciente', '$date_birth', '$edad', '$sexo', '$idDireccion')";
+                        $ejecutadoPersona = mysqli_query($conex,$sqlPersona);
+                        if (!$ejecutadoPersona) {
+                            throw new Exception("Error al insertar en la tabla Persona: " . mysqli_error($conex));
+                        }
+                    }
+            
+
+                    // Enviando TELEFONO
+                    $sqlTelefono = "INSERT INTO telefono (Nro_Telf, CI) VALUES ('$telf_paciente', '$documento_id')";
+                    $ejecutadoTelefono = mysqli_query($conex,$sqlTelefono);
+                    if (!$ejecutadoTelefono) {
+                        throw new Exception("Error al insertar en la tabla Telefono" . mysqli_error($conex));
+                    }
                 
-
-
-                        
-                // Enviando DIRECCION
-                $sqlDireccion = "INSERT INTO Direccion (ID_Parroquia, Localizacion, Calle, Sector, Nro_Casa) VALUES ('$parroquia', '$localizacion', '$calle', '$sector', '$nro_casa')";
-                $ejecutadoDireccion = mysqli_query($conex,$sqlDireccion);
-                if (!$ejecutadoDireccion) {
-                    throw new Exception("Error al insertar en la tabla Direccion" . mysqli_error($conex));
+            
+                    // Enviando CORREO
+                    $sqlCorreo = "INSERT INTO correo (Correo, CI) VALUES ('$email_paciente', '$documento_id')";
+                    $ejecutadoCorreo = mysqli_query($conex,$sqlCorreo);
+                    if (!$ejecutadoCorreo) {
+                        throw new Exception("Error al insertar en la tabla Correo" . mysqli_error($conex));
+                    }
+                
                 }
 
+                // Confirmar que cedula paciente no este registrada
+                    $buscar_ci_paciente = $user->buscarExistencia("paciente", "CIP='".$documento_id."'");
+
+                if(!$buscar_ci_paciente){
+                    
+                    // Enviando PACIENTE
+                    $sql_Paciente = "INSERT INTO paciente (CIP) VALUES ('$documento_id')";
+                    $ejecutado_Paciente = mysqli_query($conex,$sql_Paciente);
+                    if (!$ejecutado_Paciente) {
+                        throw new Exception("Error al insertar en la tabla Empleado" . mysqli_error($conex));
+                    }
 
 
-                        
-                // Enviando PERSONA
-                $buscarIdDirecion = new BySearch();
-                $buscarIdDirecion->conexion = new mysqli("localhost","root","","higea_db");
-                $resultadoDireccion = $buscarIdDirecion->buscarBY('direccion','ID_Direccion');
-                foreach ($resultadoDireccion as $filaIdDireccion) {
-                    $idDireccion = $filaIdDireccion['ID_Direccion'];
-                    $sqlPersona = "INSERT INTO persona (CI, PN, SN, TN, PA, SA, F_nac, Edad, Sexo, ID_Direccion) VALUES ('$documento_id', '$nombre1_paciente', '$nombre2_paciente', '$nombre3_paciente', '$apellido1_paciente', '$apellido2_paciente', '$date_birth', '$edad', '$sexo', '$idDireccion')";
-                    $ejecutadoPersona = mysqli_query($conex,$sqlPersona);
-                    if (!$ejecutadoPersona) {
-                        throw new Exception("Error al insertar en la tabla Persona: " . mysqli_error($conex));
+                    // Enviando MEDICO_REMITE_PACIENTE
+                    $sql_Medico_Remite_Paciente = "INSERT INTO medico_remite_paciente (ID_Medico, CIP, F_Registro, Obs) VALUES ('$id_medico', '$documento_id', '$f_registro', '$obs')";
+                    $ejecutado_Medico_Remite_Paciente = mysqli_query($conex,$sql_Medico_Remite_Paciente);
+                    if (!$ejecutado_Medico_Remite_Paciente) {
+                        throw new Exception("Error al insertar en la tabla Empleado" . mysqli_error($conex));
                     }
                 }
-        
-                // Enviando TELEFONO
-                $sqlTelefono = "INSERT INTO telefono (Nro_Telf, CI) VALUES ('$telf_paciente', '$documento_id')";
-                $ejecutadoTelefono = mysqli_query($conex,$sqlTelefono);
-                if (!$ejecutadoTelefono) {
-                    throw new Exception("Error al insertar en la tabla Telefono" . mysqli_error($conex));
+                else{
+                    echo "<script>
+                    alert('Paciente ya se encuentra registrado.');
+                    window.location.href = '../registro-paciente.php';
+                    </script>";
                 }
-            
-        
-                // Enviando CORREO
-                $sqlCorreo = "INSERT INTO correo (Correo, CI) VALUES ('$email_paciente', '$documento_id')";
-                $ejecutadoCorreo = mysqli_query($conex,$sqlCorreo);
-                if (!$ejecutadoCorreo) {
-                    throw new Exception("Error al insertar en la tabla Correo" . mysqli_error($conex));
-                }
-            
-
-                
-                // Enviando PACIENTE
-                $sql_Paciente = "INSERT INTO paciente (CIP) VALUES ('$documento_id')";
-                $ejecutado_Paciente = mysqli_query($conex,$sql_Paciente);
-                if (!$ejecutado_Paciente) {
-                    throw new Exception("Error al insertar en la tabla Empleado" . mysqli_error($conex));
-                }
-
-
-
-                // Enviando MEDICO_REMITE_PACIENTE
-                $sql_Medico_Remite_Paciente = "INSERT INTO medico_remite_paciente (ID_Medico, CIP, F_Registro, Obs) VALUES ('$id_medico', '$documento_id', '$f_registro', '$obs')";
-                $ejecutado_Medico_Remite_Paciente = mysqli_query($conex,$sql_Medico_Remite_Paciente);
-                if (!$ejecutado_Medico_Remite_Paciente) {
-                    throw new Exception("Error al insertar en la tabla Empleado" . mysqli_error($conex));
-                }
-
-
-            
-               
-        
-        
-          
-                
-
+    
                 // Mostramos un mensaje de éxito utilizando una ventana emergente de alerta de JavaScript.
                 // Después de que el usuario haga clic en el botón "Aceptar", lo redirigimos a otra página.
                 echo "<script>
@@ -173,5 +178,4 @@
                 window.location.href = '../registro-paciente.php';
                 </script>";
 
-           
 ?>
