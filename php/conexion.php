c908cc861f306eeac0b40f4e90f3dbb465548b58
Ramón R. Bastardo M. <rrbastardom2011@hotmail.com>
2024-01-29 00:27:18 -0400

    Agregado funciones de utilidad a conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -16,4 +16,41 @@ class CodeaDB{
             return $resultado->fetch_all(MYSQLI_ASSOC);
         return false;
     }
+
+    // BUSCAR x BY
+    // Utilizar esta funcion para extraer un valor de la BDD y utilizar en otras funciones
+    // Esta funcion retorna el ultimo valor registrado en la tabla
+    public function buscarBY($tabla, $columna)
+    {
+        $resultado = $this->conexion->query("SELECT * FROM $tabla ORDER BY $columna DESC LIMIT 1") or die($this->conexion->error);
+        if($resultado)
+            return $resultado->fetch_all(MYSQLI_ASSOC);
+        return false;
+    }
+
+
+    // BUSCAR ONE CELL
+    // Utilizar esta funcion para extraer un solo valor de una casilla en especifico de la BDD
+    // Esta funcion retorna el ultimo valor registrado en la tabla
+    public function buscarONE($tabla, $condicion, $columna)
+    {
+        $resultado = $this->conexion->query("SELECT * FROM $tabla ORDER BY $condicion DESC LIMIT 1") or die($this->conexion->error);
+        if($resultado){
+            $data = $resultado->fetch_all(MYSQLI_ASSOC);
+            return $data[0][$columna];
+        }
+        return false;
+    }
+
+    // BUSCAR EXISTENCIA
+    // Utilizar esta funcion para determinar si existe o no un registro
+    // Esta funcion retorna verdaro si existe o falso si no existe
+    public function buscarExistencia($tabla, $condicion)
+    {
+        $resultado = $this->conexion->query("SELECT * FROM $tabla WHERE $condicion");
+        if($resultado === false){
+            die($this->conexion->error);
+        }
+        return ($resultado->num_rows == 0) ? false : true;
+    }
 }
