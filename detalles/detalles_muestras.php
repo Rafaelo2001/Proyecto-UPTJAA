e5268d8b6e98630a526833d924742c93e7438875
Ferxxo <ferjpp20@outlook.com>
2024-02-02 23:13:20 -0400

    aplicacion de estilos css a detalles y edicion de muestras

--- detalles/detalles_muestras.php
+++ detalles/detalles_muestras.php
@@ -1,15 +1,177 @@
 <?php
+    session_start();
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: 0');
 
-        include "../php/conexion.php";
-        $user = new CodeaDB();
+    if(!isset($_SESSION['username'])) {
+        header('Location: index.php');
+        exit;
+    }
 
+    include "../php/conexion.php";
+    $user = new CodeaDB();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
+    <head>
+        <title>Listado de Muestras</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/styles_higea.css">
+        <link rel="stylesheet" type="text/css" href="../css/styles_higea.css?v=1.2">
+        <link rel="icon" type="image/png" href="../images/favicon.png">
+        <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.1.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+        <link href="../css/select2.min.css" rel="stylesheet"/>
+        <script src="../js/jquery-3.7.1.js"></script>
+        <script src="../js/select2.min.js"></script>
 
-        <body>
-                <h1>Listado de Muestras</h1>
-                <table style="text-align: center;">
+        <link href="../css/sweetalert2.min.css" rel="stylesheet"/>
+        <link rel="stylesheet" type="text/css" href="../css/sweetalert2.min.css?v=1.2">
+    </head>
+
+    <body class="login-register">
+    <div class="sidebar close">
+                <div class="logo-details">
+                <img class="logo" src="../images/Logo con contorno.png" alt="Logo de Higea" width="60" height="60">
+                <img class="logo_name" src="../images/Letras.png" alt="HIGEA" width="135" height="40">
+                </div>
+                <ul class="nav-links">
+                <li>
+                        <a href="../registro-paciente.php">
+                        <i class="fi fi-sr-hospital-user"></i>
+                        <span class="link_name">Pacientes</span>
+                        </a>
+                        <ul class="sub-menu blank">
+                        <li><a class="link_name" href="../registro-paciente.php">Pacientes</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="iocn-link">
+                        <a href="#">
+                                <i class="fi fi-sr-microscope"></i>
+                                <span class="link_name">Muestras</span>
+                        </a>
+                        <i class="fi fi-sr-angle-small-down arrow"></i>
+                        </div>
+                        <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Muestras</a></li>
+                        <li><a href="../registro-citologia.php">Citología</a></li>
+                        <li><a href="../registro-biopsia.php">Biopsia</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <a href="../registro-examen.php">
+                        <i class="fi fi-sr-flask"></i>
+                        <span class="link_name">Exámenes</span>
+                        </a>
+                        <ul class="sub-menu blank">
+                        <li><a class="link_name" href="../registro-examen.php">Exámenes</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="iocn-link">
+                        <a href="#">
+                                <i class="fi fi-sr-box-open-full"></i>
+                                <span class="link_name">Insumos</span>
+                        </a>
+                        <i class="fi fi-sr-angle-small-down arrow"></i>
+                        </div>
+                        <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Insumos</a></li>
+                        <li><a href="../registro-insumo.php">Registrar</a></li>
+                        <li><a href="../visualizar-insumo.php">Visualizar</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="iocn-link">
+                        <a href="#">
+                                <i class="fi fi-sr-file-medical-alt"></i>
+                                <span class="link_name">Informes médicos</span>
+                        </a>
+                        <i class="fi fi-sr-angle-small-down arrow"></i>
+                        </div>
+                        <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Informes médicos</a></li>
+                        <li><a href="../registro-informes.php">Registrar</a></li>
+                        <li><a href="../visualizar-informe.php">Visualizar</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <a href="../registro-pagos.php">
+                        <i class="fi fi-sr-file-invoice-dollar"></i>
+                        <span class="link_name">Facturación</span>
+                        </a>
+                        <ul class="sub-menu blank">
+                        <li><a class="link_name" href="../registro-pagos.php">Facturación</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="iocn-link">
+                        <a href="#">
+                                <i class="fi fi-sr-eye"></i>
+                                <span class="link_name">Detalles</span>
+                        </a>
+                        <i class="fi fi-sr-angle-small-down arrow"></i>
+                        </div>
+                        <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Detalles</a></li>
+                        <li><a href="detalles_pacientes.php">Pacientes</a></li>
+                        <li><a href="#">Muestras</a></li>
+                        <li><a href="#">Insumos</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="iocn-link">
+                        <a href="#">
+                                <i class="fi fi-sr-info"></i>
+                                <span class="link_name">Acerca de</span>
+                        </a>
+                        <i class="fi fi-sr-angle-small-down arrow"></i>
+                        </div>
+                        <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Acerca de</a></li>
+                        <li><a href="../info_lab.html">Sobre el Lab.</a></li>
+                        <li><a href="../info_higea.html">Sobre HIGEA</a></li>
+                        <li><a href="../developers.php">Developers</a></li>
+                        <li><a href="#">Ayuda</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <a href="#">
+                        <i class="fi fi-sr-users-alt"></i>
+                        <span class="link_name">Gestión de usuarios</span>
+                        </a>
+                        <ul class="sub-menu blank">
+                        <li><a class="link_name" href="#">Gestión de usuarios</a></li>
+                        </ul>
+                </li>
+                <li>
+                        <div class="profile-details">
+                        <a href="../php/exit.php">
+                                <i class="fi fi-sr-exit"></i>
+                                <span class="link_name">Salir</span>
+                        </a>
+                        <ul class="sub-menu blank">
+                                <li><a class="link_name" href="../php/exit.php">Salir</a></li>
+                        </ul>
+                        </div>
+                </li>
+                </ul>
+        </div>
+
+        <main class="home-section">
+            <div class="home-content">
+                <i class="fi fi-sr-menu-burger bx-menu"></i>
+                <a href="../mantenimiento/php/Gestion-BDD.php"><i class="fi fi-sr-settings bx-menu"></i></a>
+            </div>
+
+            <h1 style="text-align: center;" class="table-title">Listado de Muestras</h1>
+
+            <div class="center-table">
+
+                <table style="text-align: center;" class="table" id="table">
                         <thead>
                                 <th>N° Muestra</th>
                                 <!-- <th>ID</th> -->
@@ -18,7 +180,7 @@
                                 <th>Descripción</th>
                                 <th>Fecha Entrada</th>
                                 <th>Examinado</th>
-                                <th></th>
+                                <th>Acciones</th>
                         </thead>
                         <?php 
                                 $busquedaBiopsia = $user->buscar("m_biopsia","1");
@@ -73,8 +235,8 @@
                                         echo ("<td>".$material['Descripcion_material']."</td>");
                                     }
 
-                                        $fecha = date("Y-m-d",strtotime($material['F_Entrada']));
-                                    echo ("<td><input type='date' readonly value='".$fecha."' /></td>");            
+                                        $fecha = date("d-m-Y",strtotime($material['F_Entrada']));
+                                    echo ("<td>".$fecha."</td>");            
 
                                     if($material['Examinado']){
                                         echo ("<td>Si</td>");
@@ -114,5 +276,24 @@
                                 
                         ?>
                 </table>
-        </body>
+            </div>
+        </main>
+
+        <script>
+            let arrow = document.querySelectorAll(".arrow");
+            for (var i = 0; i < arrow.length; i++) {
+            arrow[i].addEventListener("click", (e)=>{
+            let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+            arrowParent.classList.toggle("showMenu");
+            });
+            }
+            let sidebar = document.querySelector(".sidebar");
+            let sidebarBtn = document.querySelector(".bx-menu");
+            console.log(sidebarBtn);
+            sidebarBtn.addEventListener("click", ()=>{
+            sidebar.classList.toggle("close");
+            });
+        </script>
+
+    </body>
 </html>
